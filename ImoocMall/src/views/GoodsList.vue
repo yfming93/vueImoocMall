0bbf3178f9c29315fc56cbb95055b7a20241e241
袁凤鸣 <yfmingo@163.com>
2017-10-11 14:55:08 +0800

    6-2 商品列表数据渲染实现 完成

--- ImoocMall/src/views/GoodsList.vue
+++ ImoocMall/src/views/GoodsList.vue
@@ -18,8 +18,8 @@
             <dl class="filter-price">
               <dt>Price:</dt>
               <dd><a href="javascript:void(0)">All</a></dd>
-              <dd>
-                <a href="javascript:void(0)">0 - 100</a>
+              <dd v-for="(price,index) in priceFilter">
+                <a href="javascript:void(0)">{{price.startPrice}} - {{price.endPrice}}</a>
               </dd>
 
             </dl>
@@ -29,13 +29,13 @@
           <div class="accessory-list-wrap">
             <div class="accessory-list col-4">
               <ul>
-                <li>
+                <li v-for="(item,index) in goodslist">
                   <div class="pic">
-                    <a href="#"><img src="static/1.jpg" alt=""></a>
+                    <a href="#"><img v-bind:src="'static/' + item.productImg" alt=""></a>
                   </div>
                   <div class="main">
-                    <div class="name">XX</div>
-                    <div class="price">999</div>
+                    <div class="name">{{item.productName}}</div>
+                    <div class="price">{{item.productPrice}}</div>
                     <div class="btn-area">
                       <a href="javascript:;" class="btn btn--m">加入购物车</a>
                     </div>
@@ -61,17 +61,44 @@
   import NavHeader from './NavHeader.vue'
   import NavFooter from './NavFooter.vue'
   import NavBread from './NavBread.vue'
+  import axios from 'axios'
+
 
   export default {
 		data (){
 			return {
-				msg:'hello Vue!'
+				goodslist:[],
+        priceFilter:[
+          {
+            startPrice:'0.00',
+            endPrice:'500.00'
+          },
+          {
+            startPrice:'500.00',
+            endPrice:'1000.00'
+          },
+          {
+            startPrice:'1000.00',
+            endPrice:'2000.00'
+          },
+        ]
 			}
 		},
     components:{
 		  NavHeader,
       NavFooter,
       NavBread
+    },
+    mounted:function () {
+        this.getGoodsList();
+    },
+    methods:{
+        getGoodsList:function () {
+            axios.get("/goods").then((result)=>{
+              var  res = result.data;
+              this.goodslist = res.result;
+            })
+        }
     }
 	}
 </script>
