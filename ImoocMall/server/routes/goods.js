c5a42fce7ece196e6d1ad12a262a60ed21c30584
袁凤鸣 <yfmingo@163.com>
2017-10-19 15:46:43 +0800

    9-3 商品列表分页和排序功能实现(上) ok

--- ImoocMall/server/routes/goods.js
+++ ImoocMall/server/routes/goods.js
@@ -21,8 +21,30 @@ mongoose.connection.on("disconnected", function () {
 
 
 router.get("/",function (req,res,next) {
-
-  Goods.find({},function (err,doc) {
+  let page = parseInt(req.param("page"));
+  let pageSize = parseInt(req.param("pageSize"));
+  // let priceLevel = req.param("priceLevel");
+  let sort = req.param("sort");
+  let skip = (page-1)*pageSize;
+  // var priceGt = '',priceLte = '';
+  let params = {};
+  // if(priceLevel!='all'){
+  //   switch (priceLevel){
+  //     case '0':priceGt = 0;priceLte=100;break;
+  //     case '1':priceGt = 100;priceLte=500;break;
+  //     case '2':priceGt = 500;priceLte=1000;break;
+  //     case '3':priceGt = 1000;priceLte=5000;break;
+  //   }
+  //   params = {
+  //     salePrice:{
+  //       $gt:priceGt,
+  //       $lte:priceLte
+  //     }
+  //   }
+  // }
+  let goodsModel = Goods.find(params).skip(skip).limit(pageSize);
+  goodsModel.sort({'salePrice':sort});
+  goodsModel.exec(function (err,doc) {
     if (err) {
       res.json ({
         status:'1',
